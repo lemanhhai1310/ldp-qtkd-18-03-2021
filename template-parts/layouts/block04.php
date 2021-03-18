b5e6623705a275b0ed1b62dd4f3429e6bcf095ef
HAI LE MANH <hai.lemanh.1310@gmail.com>
2021-03-18 15:26:35 +0700

    update

--- template-parts/layouts/block04.php
+++ template-parts/layouts/block04.php
@@ -1,9 +1,9 @@
 <div class="block04 uk-section uk-position-relative uk-position-z-index uk-background-top-center" data-src="images/bg1.png" uk-img uk-height-viewport>
     <div class="uk-container uk-container-small block04__boxTop">
-        <div class="uk-flex-center" uk-grid>
-            <div class="uk-width-4-5@m">
+        <div class="uk-flex-center mb-42" uk-grid>
+            <div class="uk-width-4-5 uk-width-3-5@m">
                 <div class="uk-card block04__card1 uk-card-default uk-card-body uk-background-01 uk-border-rounded">
-                    <form class="uk-grid-small" uk-grid>
+                    <form class="uk-grid-small uk-grid-16-m" uk-grid>
                         <div class="uk-width-1-2@s">
                             <input class="uk-input uk-border-pill block01__input" type="text" placeholder="Họ và tên*">
                         </div>
@@ -23,6 +23,112 @@
                 </div>
             </div>
         </div>
+        <div class="uk-position-relative">
+            <div class="uk-child-width-1-1 uk-grid-66-m" uk-grid uk-scrollspy="cls: uk-animation-slide-bottom-small; target: .animation; delay: 300; repeat: true">
+                <div>
+                    <div class="uk-flex-between uk-child-width-1-3 uk-child-width-auto@m uk-grid-small" uk-grid>
+                        <div>
+                            <div class="uk-cover-container uk-background-01 block04__box1 animation" uk-scrollspy-class="uk-animation-slide-left-small">
+                                <canvas width="186" height="186"></canvas>
+                                <div class="uk-position-cover uk-padding-small">
+                                    <div>
+                                        <div class="uk-grid-5 uk-flex-center uk-flex-middle mb-10" uk-grid>
+                                            <div>
+                                                <div class="block04__box1__txt4">5</div>
+                                            </div>
+                                            <div>
+                                                <i class="fa fa-star" aria-hidden="true"></i>
+                                            </div>
+                                        </div>
+                                        <div class="block04__box1__txt5">CHẤT LƯỢNG ĐÀO TẠO</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-hidden@m">
+                            <div class="uk-cover-container uk-background-02 block04__box1">
+                                <canvas width="186" height="186"></canvas>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="uk-cover-container uk-background-01 block04__box1 animation" uk-scrollspy-class="uk-animation-slide-right-small">
+                                <canvas width="186" height="186"></canvas>
+                                <div class="uk-position-cover uk-padding-small">
+                                    <div>
+                                        <div class="block04__box1__txt3">100%</div>
+                                        <div class="block04__box1__txt1">SINH VIÊN LÀM VIỆC THỰC TẾ TẠI DOANH NGHIỆP TỪ NĂM 3</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-visible@m">
+                    <div class="uk-flex-center uk-child-width-auto@m uk-grid-small" uk-grid>
+                        <div>
+                            <div class="uk-cover-container uk-background-02 block04__box1 animation" uk-scrollspy-class="uk-animation-slide-left-small">
+                                <canvas width="186" height="186"></canvas>
+                                <div class="uk-position-cover uk-padding-small">
+                                    <div>
+                                        <div class="block04__box1__txt2">98%</div>
+                                        <div class="block04__box1__txt1">SINH VIÊN CÓ VIỆC LÀM NGAY</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="uk-cover-container">
+                                <canvas width="186" height="186"></canvas>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="uk-cover-container">
+                                <canvas width="186" height="186"></canvas>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="uk-cover-container uk-background-02 block04__box1 animation" uk-scrollspy-class="uk-animation-slide-right-small">
+                                <canvas width="186" height="186"></canvas>
+                                <div class="uk-position-cover uk-padding-small">
+                                    <div>
+                                        <div class="block04__box1__txt2">800</div>
+                                        <div class="block04__box1__txt1 block04__box1__txt1--c1">SUẤT HỌC BỔNG</div>
+                                        <div class="block04__box1__txt1">4 NĂM ĐẠI HỌC CHO TÂN SINH VIÊN</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-visible@m">
+                    <div class="uk-flex-center uk-child-width-auto@m uk-grid-small" uk-grid>
+                        <div>
+                            <div class="uk-cover-container uk-background-01 block04__box1 animation">
+                                <canvas width="186" height="186"></canvas>
+                                <div class="uk-position-cover uk-padding-small">
+                                    <div>
+                                        <div class="block04__box1__txt2">19%</div>
+                                        <div class="block04__box1__txt1">SINH VIÊN CÓ VIỆC LÀM TẠI NƯỚC NGOÀI</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-position-top-center uk-visible@m">
+                <h4 class="uk-h4 uk-text-center block04__title1">
+                    <span>ĐẠI HỌC FPT</span> <br> NHIỀU NĂM LIÊN TIẾP ĐẠT
+                </h4>
+                <img src="images/top2.png" alt="">
+                <h4 class="uk-h4 uk-text-center block04__title2 uk-margin-remove">
+                    TRƯỜNG ĐÀO TẠO <br>
+                    QUẢN TRỊ KINH DOANH <br> TỐT NHẤT VIỆT NAM
+                </h4>
+            </div>
+        </div>
+    </div>
+    <div class="uk-container">
 
     </div>
 </div>
