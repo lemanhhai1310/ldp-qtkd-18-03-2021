6ac939b433c2f9f38cd40ee0b2e388aff598c282
HAI LE MANH <hai.lemanh.1310@gmail.com>
2021-03-18 14:14:53 +0700

    XÉT TUYỂN ĐIỂM HỌC BẠ THPT TOP50 TOÀN QUỐC

--- template-parts/layouts/block04.php
+++ template-parts/layouts/block04.php
@@ -1,5 +1,28 @@
-<div class="block04 uk-position-relative uk-position-z-index uk-background-norepeat uk-background-top-center uk-background-cover" data-src="images/bg1.png" uk-img uk-height-viewport>
-    <div class="uk-container">
+<div class="block04 uk-section uk-position-relative uk-position-z-index uk-background-top-center" data-src="images/bg1.png" uk-img uk-height-viewport>
+    <div class="uk-container uk-container-small block04__boxTop">
+        <div class="uk-flex-center" uk-grid>
+            <div class="uk-width-4-5@m">
+                <div class="uk-card block04__card1 uk-card-default uk-card-body uk-background-01 uk-border-rounded">
+                    <form class="uk-grid-small" uk-grid>
+                        <div class="uk-width-1-2@s">
+                            <input class="uk-input uk-border-pill block01__input" type="text" placeholder="Họ và tên*">
+                        </div>
+                        <div class="uk-width-1-2@s">
+                            <input class="uk-input uk-border-pill block01__input" type="text" placeholder="Số điện thoại*">
+                        </div>
+                        <div class="uk-width-1-2@s">
+                            <input class="uk-input uk-border-pill block01__input" type="text" placeholder="Email">
+                        </div>
+                        <div class="uk-width-1-2@s">
+                            <input class="uk-input uk-border-pill block01__input" type="text" placeholder="Tỉnh / Thành phố">
+                        </div>
+                        <div class="uk-text-center uk-width-1-1">
+                            <button type="button" class="uk-button animation block01__btnSubmit uk-button-secondary uk-border-pill">ĐĂNG KÝ</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
     </div>
 </div>
