0af466ec0eeb8659c4ac8afe637ee1c71d40adae
HAI LE MANH <hai.lemanh.1310@gmail.com>
2021-03-18 09:11:17 +0700

    footer

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
+<!--Thời hạn xét tuyển 25/03/2021-->
+
+<!--/Thời hạn xét tuyển 25/03/2021-->
+
 <?php require "template-parts/layouts/footer.php"; ?>
